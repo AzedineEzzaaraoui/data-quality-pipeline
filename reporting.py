960dccc7ee20451116b0a8d74e94d724cf47d781
Azeddinne EZZAARAOUI <126957297+AzedineEzzaaraoui@users.noreply.github.com>
2025-07-05 21:16:02 +0100

    Add files via upload

---
+++ reporting.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from pipeline import *
+
+def plot_qualite(df_qualite):
+    df_qualite = df_qualite.set_index('colonne')
+    df_qualite[['taux_completude (%)', 'taux_unicite (%)', 'Taux_validaty(%)', 'Taux_Cohrence(%)']]\
+        .plot(kind='bar', figsize=(10,6))
+    plt.title("Qualité des colonnes")
+    plt.ylabel("Pourcentage (%)")
+    plt.ylim(0, 110)
+    plt.grid(axis='y')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    
+    plt.savefig(r"C:\Users\user\Projets_data\Pipeline_data_quality\reports.png")
+if __name__ == "__main__":
+    df = pd.read_csv(r"C:\Users\user\Projets_data\Pipeline_data_quality\Client_Data_Sample.csv", sep=';', dtype={'telephone': str})
+
+    vis=analyser_dataframe(df)  # ta fonction qui renvoie le DataFrame résumé
+    plot_qualite(vis)
